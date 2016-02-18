1d2ba7cbca6403c693471b24f7cdadaaba99f1fb
Jim <vic20087cjimlin@gmail.com>
2016-02-18 19:17:24 +0800

    navbar animation

---
+++ assets/themes/twitter/js/scroll-topbar.js
@@ -0,0 +1,24 @@
+$(window).scroll(function() {
+
+  if ($(document).scrollTop() > 50) {
+    $('.fill').addClass('shrink');
+	$('.nav').css('display', 'none');
+    if ($(window).width() > 768) {
+		$('.brand span').css('display', 'none');
+	}else{
+		$('.brand span').css('display', 'none');
+	}
+
+  } else {
+    $('.fill').removeClass('shrink');
+
+    if ($(window).width() > 768) {
+	    $('.brand span').css('display', 'inline-block');
+	    $('.nav').css('display', 'block');	
+	}else{
+	    $('.brand span').css('display', 'inline-block');
+	}
+
+  }
+
+});
