6d5a3bd8d6693edb482a913e3af73215a302addd
Jim <vic20087cjimlin@gmail.com>
2016-03-11 20:55:21 +0800

    add about me intern info

--- assets/themes/twitter/js/scroll-topbar.js
+++ assets/themes/twitter/js/scroll-topbar.js
@@ -30,3 +30,26 @@ if ($(window).width() > 1200){
 	$('.posts').removeClass('span14');
 	//$('.posts').addClass('span14');	
 }
+
+
+$( ".about-tab" ).click(function() {
+  var index = $(".about-tab").index(this);
+
+  // ===== tab notification =====
+
+  $(".flex-tabs").children().removeClass("tab-notce").css({
+  	"color": "#999cab",
+  	"border-bottom": "1px solid #e5e6ea"
+  });
+
+  $(".flex-tabs").children().eq(index).addClass("tab-notce").css({
+  	"color": "#FC7358",
+  	"border-bottom": "1px solid #fff"
+  });
+
+  // ===== panel display =====
+
+  $(".panel-content").children().css("display","none");
+  $(".panel-content").children().eq(index).css("display","block");
+
+});
