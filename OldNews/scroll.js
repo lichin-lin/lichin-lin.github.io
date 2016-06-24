eb27dc80c83005efc36f010be5c7f85b0c92956a
Jim <vic20087cjimlin@gmail.com>
2016-06-25 00:48:43 +0800

    add new feature

--- OldNews/scroll.js
+++ OldNews/scroll.js
@@ -10,31 +10,22 @@ function offset() {
   //console.log( $('.OldNews_title')[0] );
   var position = $( '.OldNews_title' ).offset();
   var offsetAmount = $( window ).width()/$( window ).height();
-  console.log("scrollY:" +  scrollY() + ", body;" +  $('body').height() + ", document:" + $(document).height() + ", screen:" + $(window).height());
   // console.log("the broswer condition: " + $( window ).width() + "& height: " + $( window ).height() );
   // console.log("left:" + position.left);
-  if( scrollY() < $( window ).height() ) {
-    //console.log('init pos:' + position.left);
-    position = topPosition - 2*scrollY()*offsetAmount;
-    //console.log('after shift:' + position);
-    $('.OldNews_title').css({
-      "left": position
-    });
-  } else if ( (scrollY() + $(window).height() + 150) > ( $(document).height() ) ){
-    console.log('final:' + position.left);
-    var b = $(window).height()/5;
-    $('.OldNews_title').css({
-      "left": 0,//(scrollY() + $(window).height() + 150 - $(document).height)
-      "bottom": 0,
-    });
+  if($(window).width() > 768){
+    if( scrollY() < $( window ).height() ) {
+      //console.log('init pos:' + position.left);
+      position = topPosition - 2*scrollY()*offsetAmount;
+      $('.OldNews_title').css({
+        "left": position
+      });
+    } else {
+      console.log('while:' + position.left);
+      $('.OldNews_title').css({
+        "left": -3*( $('body').width() )// - $('body').height() - 3* $( '.OldNews_title' ).width() )//- 2* $( '.OldNews_title' ).width())
+      });
+    }
   }
-  else {
-    console.log('while:' + position.left);
-    $('.OldNews_title').css({
-      "left": -3*( $('body').width() )// - $('body').height() - 3* $( '.OldNews_title' ).width() )//- 2* $( '.OldNews_title' ).width())
-    });
-  }
-
   $('.OldNews_container').css({
     "-webkit-transform":"translate3d(0," + -scrollY() + "px,0)",
     "-ms-transform":"translate3d(0," + -scrollY() + "px,0)",
